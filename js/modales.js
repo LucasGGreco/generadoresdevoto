972decca26334c0b6ed40e501820fc3b0e6b2241
Lucas Greco <lucas.greco129@gmail.com>
2026-05-15 16:57:19 -0300

    se suben nuevas imagenes

--- js/modales.js
+++ js/modales.js
@@ -14,33 +14,41 @@ jQuery(document).ready(function($) {
             "imagen": "img/galeria/generador3.png", 
             "descripcion": "6kva - Lugano" 
         },{ 
-            "id": "4", 
-            "imagen": "img/galeria/generador2.png", 
-            "descripcion": "6kva - Coghlan" 
-        },{ 
-            "id": "5",  
+            "id": "4",  
             "imagen": "img/galeria/generador8.png", 
             "descripcion": "106kva Gvep - Villa Del Parque" 
         },{ 
-            "id": "6", 
+            "id": "5", 
             "imagen": "img/galeria/generador6.png", 
             "descripcion": "15kva Trif - Efectos Especiales" 
         },{ 
-            "id": "7", 
+            "id": "6", 
             "imagen": "img/galeria/generador7.png", 
             "descripcion": "5kva - Embajada Venezuela" 
         },{ 
-            "id": "8",
+            "id": "7",
             "imagen": "img/galeria/generador5.png", 
             "descripcion": "7kva Honda - Mantenimiento Mensual" 
         },{ 
-            "id": "9", 
+            "id": "8", 
             "imagen": "img/galeria/generador9.png", 
             "descripcion": "Jhon deer 200kva - Cramaco Industria Nacional" 
         },{ 
-            "id": "10", 
+            "id": "9", 
             "imagen": "img/galeria/generador10.png", 
             "descripcion": "Logus 40-VDP-Back Up" 
+        },{ 
+            "id": "10", 
+            "imagen": "img/galeria/generadorAmarillo.png", 
+            "descripcion": "60kva Perkins"
+        },{ 
+            "id": "11", 
+            "imagen": "img/galeria/generadorRojo.png", 
+            "descripcion": "110kva Perkins" 
+        },{ 
+            "id": "12", 
+            "imagen": "img/galeria/generadorVerde.png", 
+            "descripcion": "40kva MWM" 
         },
     ];
   
@@ -52,47 +60,24 @@ jQuery(document).ready(function($) {
         const elementoDiv = document.createElement("div");
         elementoDiv.className = "col-12 col-md-6 col-lg-4 my-3 mx-0";
 
-        if (element.id == 10) {
-            elementoDiv.innerHTML = `
-            <div class="card" data-toggle="modal" data-target="#modal${item.id}">
-                <div class="card-img">
-                    <img src="${item.imagen}" alt="Generadores Devoto">
-                    <div class="overlay-text">${item.descripcion}</div>
-                </div>
+        elementoDiv.innerHTML = `
+        <div class="card" data-toggle="modal" data-target="#modal${item.id}">
+            <div class="card-img">
+                <img src="${item.imagen}" alt="Generadores Devoto">
+                <div class="overlay-text">${item.descripcion}</div>
             </div>
-    
-            <div class="modal fade" id="modal${item.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-                    <div class="modal-content bg-transparent">
-                        <div class="modal-body text-center">
-                            <video controls>
-                                <source src="mp4/logus.mp4" type="video/mp4">
-                            </video>
-                        </div>
+        </div>
+
+        <div class="modal fade" id="modal${item.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                <div class="modal-content bg-transparent">
+                    <div class="modal-body text-center">
+                        <img src="${item.imagen}" alt="${item.descripcion}" class="img-fluid">
                     </div>
                 </div>
             </div>
-            `;
-        }else{
-            elementoDiv.innerHTML = `
-            <div class="card" data-toggle="modal" data-target="#modal${item.id}">
-                <div class="card-img">
-                    <img src="${item.imagen}" alt="Generadores Devoto">
-                    <div class="overlay-text">${item.descripcion}</div>
-                </div>
-            </div>
-    
-            <div class="modal fade" id="modal${item.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-                    <div class="modal-content bg-transparent">
-                        <div class="modal-body text-center">
-                            <img src="${item.imagen}" alt="${item.descripcion}" class="img-fluid">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            `;
-        }
+        </div>
+        `;
 
         // Agrega el div al contenedor
         galeria.appendChild(elementoDiv);
